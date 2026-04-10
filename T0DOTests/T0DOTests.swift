d31bd45a332349648715717a24e9c102e4104120
Sós Dávid <davidk@Sos-MacBook-Air.local>
2026-04-10 21:36:27 +0200

    Ds store fent volt

---
+++ T0DOTests/T0DOTests.swift
@@ -0,0 +1,19 @@
+//
+//  T0DOTests.swift
+//  T0DOTests
+//
+//  Created by Sós Dávid on 2026. 03. 31..
+//
+
+import Testing
+@testable import T0DO
+
+struct T0DOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ T0DOUITests/T0DOUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  T0DOUITestsLaunchTests.swift
+//  T0DOUITests
+//
+//  Created by Sós Dávid on 2026. 03. 31..
+//
+
+import XCTest
+
+final class T0DOUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
